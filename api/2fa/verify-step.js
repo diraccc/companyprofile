188a0195677cdc6fbf087c03c55ccec63b878296
company <supportdirac@gmail.com>
2026-06-04 10:26:21 +0700

    Add files via upload

--- api/2fa/verify-step.js
+++ api/2fa/verify-step.js
@@ -183,11 +183,13 @@ function setCors(reqOrRes, maybeRes) {
 
   if (origin && allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Vary", "Origin");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
+  res.setHeader("Vary", "Origin");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Cache-Control", "no-store");
 }
 
 function sign(data, secret) {
